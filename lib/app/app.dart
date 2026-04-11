72836779910b8766c1641098c1d71f06f139e232
speechgenius <shaabandaudi@gmail.com>
2026-04-11 22:19:12 +0300

    app router and main file edited

---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:consistency/app/router.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget{
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+return MaterialApp.router(
+  routerConfig: router,
+);
+  }
+}
